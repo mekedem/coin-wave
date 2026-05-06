89a5d820c35619de79b29ee147e4bd83acdd3053
Mekedem Getaneh <mekedemgetaneh@gmail.com>
2026-05-06 14:04:51 +0300

    implement: coin overview and trends ui and function using the fetcher util

--- next.config.ts
+++ next.config.ts
@@ -4,8 +4,12 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'assets.coingecko.com',
+        protocol: "https",
+        hostname: "assets.coingecko.com",
+      },
+      {
+        protocol: "https",
+        hostname: "coin-images.coingecko.com",
       },
     ],
   },
